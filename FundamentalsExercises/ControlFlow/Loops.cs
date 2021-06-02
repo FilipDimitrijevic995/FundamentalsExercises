72ef3e3cf35d3582963242da2dfd2a555c1003cb
Filip Dimitrijevic <dimitrijevicfilip995@gmail.com>
2021-06-02 17:51:38 +0200

    Add project files.

---
+++ FundamentalsExercises/ControlFlow/Loops.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundamentalsExercises.ControlFlow
+{
+    class Loops
+    {
+        /*
+         1- Write a program to count how many numbers between 1 and 100 are divisible by 3 with no remainder. 
+            Display the result on the console.
+        */
+        public void Exercise1()
+        {
+        }
+
+        /*
+          2-  Write a program and continuously ask the user to enter a number. The loop terminates when the user 
+              enters “ok". Calculate the sum of all the previously entered numbers and display it on the console.
+        */
+        public void Exercise2()
+        {
+        }
+
+        /*
+           3- Write a program which takes a single argument from the console, computes the factorial and prints the 
+              value on the console. For example, if the user enters 5, the program should calculate 5 x 4 x 3 x 2 x 1 
+              and display it as 5! = 120.
+        */
+        public void Exercise3()
+        {
+        }
+
+        /*
+           4- Write a program that picks a random number between 1 and 10. Give the user 4 chances to guess the number. 
+              If the user guesses the number, display “You won". Otherwise, display “You lost".
+         */
+        public void Exercise4()
+        {
+        }
+
+        /*
+           5- Write a program and ask the user to enter a series of numbers separated by comma. Find the maximum of the 
+              numbers and display it on the result. For example, if the user enters “5, 3, 8, 1, 4", the program should 
+              display 8 on the console.
+        */
+        public void Exercise5()
+        {
+        }
+    }
+}
---
+++ FundamentalsExercises/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace FundamentalsExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var conditionals = new ControlFlow.Conditionals();
+            //conditionals.Exercise1();
+            //conditionals.Exercise2();
+            //conditionals.Exercise3();
+            //conditionals.Exercise4();
+
+            var loops = new ControlFlow.Loops();
+            //loops.Exercise1();
+            //loops.Exercise2();
+            //loops.Exercise3();
+            //loops.Exercise4();
+            //loops.Exercise5();
+
+            var arraysAndLists = new ArraysAndLists.ArraysAndLists();
+            //arraysAndLists.Exercise1();
+            //arraysAndLists.Exercise2();
+            //arraysAndLists.Exercise3();
+            //arraysAndLists.Exercise4();
+            //arraysAndLists.Exercise5();
+
+            var strings = new WorkingWithText.Strings();
+            //strings.Exercise1();
+            //strings.Exercise2();
+            //strings.Exercise3();
+            //strings.Exercise4();
+            //strings.Exercise5();
+
+            Console.ReadKey();
+        }
+    }
+}
